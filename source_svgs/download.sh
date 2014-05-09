9fb8eaeedcf743da99e8d1d8be9a198dcb330880
Manuel Aristarán <jazzido@jazzido.com>
2014-05-08 19:58:57 -0500

    comment

--- source_svgs/download.sh
+++ source_svgs/download.sh
@@ -1,4 +1,7 @@
 #!/bin/bash
+
+# baja los mapas de fracciones y radios (SVG) que están en http://www.opex.sig.indec.gov.ar/codgeo
+
 for id in `ogr2ogr -f CSV /vsistdout/ ../departamentos_shp/paisxdpto2010.shp -sql "select LINK from paisxdpto2010"`; do
     wget -O fracciones/$id.svg "http://www.opex.sig.indec.gov.ar/codgeo/mapas/link2svg.php?link=$id&width=1350&height=1500";
 done
