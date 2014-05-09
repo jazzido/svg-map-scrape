468e5539b27d8aa087fbcb0a5d55ffd6dd23eaae
Manuel Aristarán <jazzido@jazzido.com>
2014-05-09 15:18:57 -0500

    del

--- source_svgs/radios/download.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-
-for radio_id in `cat ../fracciones/*svg | grep 'tipo="Fracción' | sed -nE 's/.*clave_unica="([^"]+)".*/\1/p' | sort | uniq`; do
-    wget -O $radio_id.svg "http://www.opex.sig.indec.gov.ar/codgeo/mapas/link2svg.php?link=$radio_id&width=1350&height=1500";
-done
