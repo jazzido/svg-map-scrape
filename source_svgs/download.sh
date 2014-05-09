5e979cb64c6d7eff7979992d9f869aa3f9ea0e73
Manuel Aristarán <jazzido@jazzido.com>
2014-05-08 19:30:29 -0500

    download scripts

---
+++ source_svgs/download.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+for id in `ogr2ogr -f CSV /vsistdout/ ../departamentos_shp/paisxdpto2010.shp -sql "select LINK from paisxdpto2010"`; do
+    wget -O fracciones/$id.svg "http://www.opex.sig.indec.gov.ar/codgeo/mapas/link2svg.php?link=$id&width=1350&height=1500";
+done
+
+for radio_id in `cat fracciones/*svg | grep 'tipo="Fracción' | sed -nE 's/.*clave_unica="([^"]+)".*/\1/p' | sort | uniq`; do
+    wget -O radios/$radio_id.svg "http://www.opex.sig.indec.gov.ar/codgeo/mapas/link2svg.php?link=$radio_id&width=1350&height=1500";
+done
--- source_svgs/fracciones/download.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-for id in `ogr2ogr -f CSV /vsistdout/ ../../departamentos_shp/paisxdpto2010.shp -sql "select LINK from paisxdpto2010"`; do
-    wget -O $id.svg "http://www.opex.sig.indec.gov.ar/codgeo/mapas/link2svg.php?link=$id&width=1350&height=1500";
-done
