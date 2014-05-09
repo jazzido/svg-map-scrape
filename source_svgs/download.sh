ca2ff8387b10a23364588bcde0d6b145442f5d0b
Manuel Aristarán <jazzido@jazzido.com>
2014-05-09 15:18:53 -0500

    better variables

--- source_svgs/download.sh
+++ source_svgs/download.sh
@@ -2,10 +2,10 @@
 
 # baja los mapas de fracciones y radios (SVG) que están en http://www.opex.sig.indec.gov.ar/codgeo
 
-for id in `ogr2ogr -f CSV /vsistdout/ ../departamentos_shp/paisxdpto2010.shp -sql "select LINK from paisxdpto2010"`; do
-    wget -O fracciones/$id.svg "http://www.opex.sig.indec.gov.ar/codgeo/mapas/link2svg.php?link=$id&width=1350&height=1500";
+for dpto_id in `ogr2ogr -f CSV /vsistdout/ ../departamentos_shp/paisxdpto2010.shp -sql "select LINK from paisxdpto2010"`; do
+    wget -O - "http://www.opex.sig.indec.gov.ar/codgeo/mapas/link2svg.php?link=$dpto_id&width=1350&height=1500" | tidy -xml > fracciones/$dpto_id.svg;
 done
 
-for radio_id in `cat fracciones/*svg | grep 'tipo="Fracción' | sed -nE 's/.*clave_unica="([^"]+)".*/\1/p' | sort | uniq`; do
-    wget -O radios/$radio_id.svg "http://www.opex.sig.indec.gov.ar/codgeo/mapas/link2svg.php?link=$radio_id&width=1350&height=1500";
+for fraccion_id in `cat fracciones/*svg | grep 'tipo="Fracción' | sed -nE 's/.*clave_unica="([^"]+)".*/\1/p' | sort | uniq`; do
+    wget -O - "http://www.opex.sig.indec.gov.ar/codgeo/mapas/link2svg.php?link=$fraccion_id&width=1350&height=1500" | tidy -xml > radios/$fraccion_id.svg ;
 done
